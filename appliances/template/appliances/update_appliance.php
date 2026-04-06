10f6302e3dcf8c0a766d5a27c11e751faa665ec0
ayjay-9 <ayobanjoemmanuel1@gmail.com>
2026-04-06 14:18:22 +0100

    Add update page

---
+++ appliances/template/appliances/update_appliance.php
@@ -0,0 +1,27 @@
+<!-- Assignment 2 -->
+<!-- Name: Emmanuel Ayobanjo -->
+<!-- Student ID: 3173959 -->
+
+<?php
+    // Connect to the database like in the add_appliance.php file
+    require_once '../../../config.php';
+    $con = mysqli_connect($host, $username, $password, $dbname);
+    // Check connection to the database. If the connection fails, terminate the script and display an error message indicating the reason for the failure. This ensures that any issues with the database connection are promptly identified and handled gracefully.
+    if (!$con) {
+        die("Connection failed: " . mysqli_connect_error());
+    }
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Update Appliance</title>
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" integrity="sha384- EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <link rel="stylesheet" href="../../static/appliances/style.css" type="text/css">
+</head>
+<body>
+</body>
+</html>
